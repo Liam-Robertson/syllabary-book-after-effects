75e7414b04402cb47f4dcf922e92eb2daec25c62
Liam Robertson <liam.robertson@fdmgroup.com>
2021-09-20 02:32:59 +0100

    finished prototype

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    apiKey: "AIzaSyBV1EGccExIepMLmYHxmOfvQ2fvBmoqlNc",
+    authDomain: "the-syllabary.firebaseapp.com",
+    projectId: "the-syllabary",
+    storageBucket: "the-syllabary.appspot.com",
+    messagingSenderId: "34208514522",
+    appId: "1:34208514522:web:3cecd99e253bb359d240e3",
+    measurementId: "G-EWTQWBYS4K"
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false, 
+  firebase: {
+    apiKey: "AIzaSyBV1EGccExIepMLmYHxmOfvQ2fvBmoqlNc",
+    authDomain: "the-syllabary.firebaseapp.com",
+    projectId: "the-syllabary",
+    storageBucket: "the-syllabary.appspot.com",
+    messagingSenderId: "34208514522",
+    appId: "1:34208514522:web:3cecd99e253bb359d240e3",
+    measurementId: "G-EWTQWBYS4K"
+  }
 };
 
 /*
